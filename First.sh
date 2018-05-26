5ee5f1b8ace4fb68a98f634dc3441a4a51c8acc4
ram <rm@gmail.com>
2018-05-26 17:44:08 +0530

    added Maven

--- First.sh
+++ First.sh
@@ -16,6 +16,14 @@ wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-
 tar -xvzf jdk-8u171-linux-x64.tar.gz
 
 
+mkdir /opt/maven
+
+cd /opt/maven
+wget http://www-eu.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
+
+tar -xvzf apache-maven-3.5.3-bin.tar.gz
+
+
 
 
 
