31e9da32fd8b6cfc40a9f69e72afb6941557314f
root <root@localhost.localdomain>
2018-05-26 17:29:18 +0530

    added on 26th May

---
+++ First.sh
@@ -0,0 +1,21 @@
+# Installing Wget 
+
+yum install wget -y
+
+
+mkdir /opt/java
+
+cd /opt/java
+
+#Downloading wget 
+
+
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
+
+#untar file
+tar -xvzf jdk-8u171-linux-x64.tar.gz
+
+
+
+
+
